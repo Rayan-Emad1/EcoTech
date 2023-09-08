e75912bc114933b495f8205de7814586f29b7c38
Rayan <rayanemad508@gmail.com>
2023-09-08 13:06:17 +0300

    BE <> added routes

--- Server/routes/auth.routes.js
+++ Server/routes/auth.routes.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const authController = require("../controllers/auth.controllers");
 
 router.post("/check_email", authController.checkEmail)
+router.post("/register_user", authController.registerUser)
+router.post("/verify", authController.verify)
+router.post("/login", authController.login)
 
 module.exports = router;
