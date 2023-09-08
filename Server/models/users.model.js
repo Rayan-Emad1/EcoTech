98bba3647de9eac897d7830e9221180c855c4ad3
Rayan <rayanemad508@gmail.com>
2023-09-08 15:53:54 +0300

    BE <> account expires if not verified  within 5min

--- Server/models/users.model.js
+++ Server/models/users.model.js
@@ -1,4 +1,3 @@
-const { string } = require("joi");
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
@@ -8,12 +7,19 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
   birthday: { type: Date, default: null },
   address: { type: String, default: null },
-  notification_token: {type:String, default:null },
+  notification_token: { type: String, default: null },
   verificationCode: { type: String, default: null },
+  isVerified: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now },
 });
 
-userSchema.index({ createdAt: 1 }, { expireAfterSeconds: 300 }); 
+userSchema.index(
+  { createdAt: 1 },
+  {
+    expireAfterSeconds: 0,
+    partialFilterExpression: { $and: [{ isVerified: false }, { createdAt: { $lt: new Date(Date.now() - 300000) } }] },
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
