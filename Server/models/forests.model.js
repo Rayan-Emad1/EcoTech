d30c2dd374968cda25cd5fad54df9f698f04bee9
Rayan <rayanemad508@gmail.com>
2023-09-08 22:34:50 +0300

    BE <> added the forest schema

--- Server/models/forests.model.js
+++ Server/models/forests.model.js
@@ -6,31 +6,55 @@ const hourlyDataSchema = new mongoose.Schema({
     hour: { type: Number, required: true },
     day: { type: Number, required: true },
     source: { type: String, enum: ["real", "predicted"], required: true },
-  });
+});
   
-  const dailyDataSchema = new mongoose.Schema({
+const dailyDataSchema = new mongoose.Schema({
     value: { type: Number, required: true },
     month: { type: Number, required: true },
     day: { type: Number, required: true },
     source: { type: String, enum: ["real", "predicted"], required: true },
-  });
+});
 
-  const temperatureSchema = new mongoose.Schema({
+const temperatureSchema = new mongoose.Schema({
     hourly: [hourlyDataSchema],
     daily: [dailyDataSchema],
-  });
+});
   
-  const windSchema = new mongoose.Schema({
+const windSchema = new mongoose.Schema({
     hourly: [hourlyDataSchema],
     daily: [dailyDataSchema],
-  });
-  
-  const humiditySchema = new mongoose.Schema({
+});
+
+const humiditySchema = new mongoose.Schema({
     hourly: [hourlyDataSchema],
     daily: [dailyDataSchema],
-  });
-  
-  
-  
-  module.exports = Forest;
-  
+});
+
+const forestsSchema = new mongoose.Schema({
+  location: {
+    type: {
+      type: String,
+      enum: ["Point"],
+      default: "Point",
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+    },
+  },
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  temperature: temperatureSchema,
+  wind: windSchema,
+  humidity: humiditySchema,
+  fire_alarm: { type: Boolean, default: false },
+});
+
+
+
+const Forest = mongoose.model("Forest", forestsSchema);
+
+
+
+
+module.exports = Forest;
