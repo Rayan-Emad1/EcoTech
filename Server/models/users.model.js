a612a0d3d89962fb5e41a230005c479db721488e
Rayan <rayanemad508@gmail.com>
2023-09-08 04:50:34 +0300

    BE <> added user model

---
+++ Server/models/users.model.js
@@ -0,0 +1,20 @@
+const { string } = require("joi");
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  birthday: { type: Date, default: null },
+  address: { type: String, default: null },
+  token: {type:string, default:null },
+  verificationCode: { type: String, default: null },
+  createdAt: { type: Date, default: Date.now },
+});
+
+userSchema.index({ createdAt: 1 }, { expireAfterSeconds: 300 }); 
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
