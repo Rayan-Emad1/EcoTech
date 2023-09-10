d899d12be93f8d68275a912f32f1829cc8402b59
Rayan <rayanemad508@gmail.com>
2023-09-11 00:06:27 +0300

    BE <> changed middleware

--- Server/middlewares/auth.middleware.js
+++ Server/middlewares/auth.middleware.js
@@ -1,19 +1,20 @@
 const jwt = require("jsonwebtoken");
-require('dotenv').config();
+const jwtSecret = process.env.JWT_SECRET;
 
-const authMiddleware = (req, res, next) => {
-    const token = req.headers.authorization?.split(" ")[1];
-    if(!token) return res.status(401).send({message: "Unauthorized"});
-    try {
+const auth = async (req, res, next) => {
+  const token = req.header("Authorization").replace("Bearer ", "");
 
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded
-        next()
+  if (!token) {
+    return res.status(401).send("Access denied");
+  }
 
-    } catch (error) {
-        console.log(error);
-        res.status(401).send({message: "Unauthorized"})
-    }
-}
+  try {
+    const decoded = jwt.verify(token, jwtSecret);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(400).send("Invalid token");
+  }
+};
 
-module.exports=authMiddleware;
+module.exports = auth;
