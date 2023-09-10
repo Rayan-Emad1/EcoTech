c2ac6b02b387b7b5f89615e9f794a39e03234a10
Rayan <rayanemad508@gmail.com>
2023-09-10 16:48:16 +0300

    BE <> fixed update data field daily

--- Server/Logic/functions.js
+++ Server/Logic/functions.js
@@ -1,82 +1,72 @@
-const Forest = require("../models/forests.model");
+const updateDataField = (data_field, new_data) => {
+  // console.log(`Before update ${data_field}`);
+  // console.log("=================================================");
 
-const calculateDailyAverage = async (data_field, day) => {
+  if (!data_field || !data_field.hourly) {
+    console.error(`Invalid data field or missing 'hourly' property.`);
+    return;
+  }
 
-    const forest = await Forest.findOne({ _id: data_field._id });
-    if (!forest) {
-        console.error(`Forest not found for data field.`);
-        return;
-    }
+  new_data.forEach((new_data_item) => {
+    const { hour, day, source, value } = new_data_item;
 
-    const data_type = data_field.type; 
-    const daily_data_from_Db = forest[data_type].hourly.filter(
-        (data) => data.day === day
+    const existing_data = data_field.hourly.find(
+      (data) => data.hour === hour && data.day === day
     );
 
-
-    const real_data = daily_data_from_Db.filter((data) => data.source === 'real');
-    const sum = real_data.reduce((acc, data) => acc + data.value, 0);
-    const average = sum / real_data.length;
-
-
-    const timestamp = new Date(daily_data_from_Db[0].timestamp);
-    const month = timestamp.getMonth() + 1; 
-
-
-    forest[data_type].daily.push({
-        value: average,
-        day: day,
-        month: month,
+    if (existing_data) {
+      // console.log(`Updating existing data for hour ${hour} on ${day}`);
+      existing_data.value = value;
+      existing_data.source = source;
+      existing_data.timestamp = new Date();
+    } else {
+      // console.log(`Adding new data for hour ${hour} on ${day}`);
+      data_field.hourly.push({
+        value,
+        hour,
+        day,
+        source,
         timestamp: new Date(),
-    });
+      });
+    }
+  });
 
-    forest[data_type].hourly = [];
-    
-    await forest.save();
+  const { hour, source, day } = new_data[0];
+  if (hour === 24 && source === "real") {
+    updateDataFieldDaily(data_field, day);
+  }
 
+  // console.log("=================================================");
+  // console.log(`After update ${data_field}`);
 };
 
-const updateDataField = async (data_field, new_data) => {
-    console.log("Data before update");
-    console.log(data_field);
-    console.log("=================================================");
+const updateDataFieldDaily = (data_field, day) => {
+  // console.log(data_field);
 
-    if (!data_field || !data_field.hourly) {
-        console.error(`Invalid data field or missing 'hourly' property.`);
-        return;
-    }
+  const daily_values = data_field.hourly.filter((data) => data.day === day);
 
+  const real_data = daily_values.filter((data) => data.source === "real");
+  const sum = real_data.reduce((acc, data) => acc + data.value, 0);
+  const average = sum / real_data.length;
 
-    if (new_data[0].hour === 24 && new_data[0].source === "real") {
-        await calculateDailyAverage(data_field, new_data[0].day );
-    }
-    
+  // console.log("====================================");
+  // console.log(sum);
+  // console.log(average);
+  // console.log(`Before update ${data_field.daily}`);
+  // console.log("====================================");
 
-    new_data.forEach((newDataItem) => {
-        const { hour, day, source, value } = newDataItem;
+  const timestamp = new Date(real_data[0].timestamp);
+  const month = timestamp.getMonth();
 
-        const existingData = data_field.hourly.find((data) => data.hour === hour && data.day === day);
+  data_field.daily.push({
+    value: average,
+    day: day,
+    month: month,
+    timestamp: new Date(),
+    source: real,
+  });
 
-        if (existingData) {
-            console.log(`Updating existing data for hour ${hour} on ${day}`);
-            existingData.value = value;
-            existingData.source = source;
-            existingData.timestamp = new Date();
-        } else {
-            console.log(`Adding new data for hour ${hour} on ${day}`);
-            data_field.hourly.push({
-                value,
-                hour,
-                day,
-                source,
-                timestamp: new Date(),
-            });
-        }
-    });
-    console.log("=================================================");
-
-    console.log("Data after update");
-    console.log(data_field);
+  // console.log(`After update ${data_field.daily}`);
 };
 
-module.exports = {updateDataField };
+module.exports = { updateDataField };
