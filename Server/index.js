dc1606c874bec79f17eb7f19680f6498ff60c85f
Rayan <rayanemad508@gmail.com>
2023-09-11 00:04:49 +0300

    BE <> added limits to access auth routes

--- Server/index.js
+++ Server/index.js
@@ -1,11 +1,19 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 8000;
 const mongooseConnect = require("./configs/mongoDB.connect");
+const rateLimit = require("express-rate-limit");
 
 app.use(cors());
 app.use(express.json());
+
+const authLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000,     // 10 minutes
+  max: 5,                      // Limit each IP to 5 requests per windowMs
+});
+
+app.use("/auth", authLimiter);
 
 const authRouter = require("./routes/auth.routes");
 app.use("/auth", authRouter);
@@ -20,5 +28,4 @@ app.listen(PORT, (err) => {
   }
   console.log(`server running on port: ${PORT} `);
   mongooseConnect();
-
 });
