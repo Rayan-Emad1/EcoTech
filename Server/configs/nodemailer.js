8701deaf2be11f6cfd66839ca24b3691ff6d7801
Rayan <rayanemad508@gmail.com>
2023-09-11 00:24:41 +0300

    BE <> refactored node mailer function

--- Server/configs/nodemailer.js
+++ Server/configs/nodemailer.js
@@ -1,57 +1,50 @@
-const nodemailer = require('nodemailer');
-const Mailgen = require('mailgen');
-require('dotenv').config();
-const { EMAIL, PASSWORD } =  process.env
-
+const nodemailer = require("nodemailer");
+const Mailgen = require("mailgen");
+require("dotenv").config();
+const { EMAIL, PASSWORD } = process.env;
 
 const sendVerificationEmail = (UserName, userEmail, verificationCode) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: EMAIL,
+      pass: PASSWORD,
+    },
+  });
 
-  let config = {
-      service : 'gmail',
-      auth : {
-          user: EMAIL,
-          pass: PASSWORD
-      }
-  }
+  const mailGenerator = new Mailgen({
+    theme: "default",
+    product: {
+      name: "EcoTech",
+      link: "https://mailgen.js/",
+    },
+  });
 
-  let transporter = nodemailer.createTransport(config);
-  let MailGenerator = new Mailgen({
-      theme: "default",
-      product : {
-          name: "EcoTech",
-          link : 'https://mailgen.js/'
-      }
-  })
-
-  let response = {
+  const emailContent = {
     body: {
       name: UserName,
-      intro: `Welcome to EcoTech! We\'re very excited to have you on board. To get started with EcoTech, please verify your email using:\n\n\n Verification Code: ${verificationCode}`,
-      outro: 'Need help, or have questions? Just reply to this email, we\'d love to help.'
-    }
+      intro: `Welcome to EcoTech! We're very excited to have you on board. To get started with EcoTech, please verify your email using the following verification code: ${verificationCode}`,
+      outro:
+        "Need help or have questions? Just reply to this email; we'd love to help.",
+    },
   };
 
-  let mail = MailGenerator.generate(response)
+  const emailTemplate = mailGenerator.generate(emailContent);
 
-  let message = {
-    from : EMAIL,
-    to : userEmail,
-    subject: "EcoTech: Email verification",
-    html: mail
-  }
+  const message = {
+    from: EMAIL,
+    to: userEmail,
+    subject: "EcoTech: Email Verification",
+    html: emailTemplate,
+  };
 
-  transporter.sendMail(message)
-    
-}
+  transporter.sendMail(message, (error, info) => {
+    if (error) {
+      console.error("Error sending verification email:", error);
+    } else {
+      console.log("Verification email sent:", info.response);
+    }
+  });
+};
 
-module.exports = {sendVerificationEmail}
-
-// action: {
-//   instructions: 'To get started with EcoTech, please verify your email using:',
-//   button: {
-//     color: '#22BC66', 
-//     text: 'Verify Email',
-//     link: verificationCode,
-//     fallback: 'If you have trouble clicking the "Verify Email" button, copy and paste the following URL into your browser: ' + verificationCode,
-//   },
-// },
+module.exports = { sendVerificationEmail };
