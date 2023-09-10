0318d7794685c90485745e698e61321434f0de5a
Rayan <rayanemad508@gmail.com>
2023-09-11 00:11:56 +0300

    BE <> added Joi verification and fixed spacing

--- Server/controllers/auth.controllers.js
+++ Server/controllers/auth.controllers.js
@@ -2,142 +2,154 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("../models/users.model");
 const { sendVerificationEmail } = require("../configs/nodemailer");
+const Joi = require("joi");
+
+const jwtSecret = process.env.JWT_SECRET;
+
+const registerSchema = Joi.object({
+  first_name: Joi.string().required(),
+  last_name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(8).required(),
+  birthday: Joi.date(),
+});
+
+const loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().min(8).required(),
+});
 
 const checkEmail = async (req, res) => {
-    try {
-      const { email } = req.body;
-      console.log(req.body);
-        const existing_user = await User.findOne({ email });
-  
-      if (existing_user && existing_user.isVerified ) {
-        return res.status(200).json({ isUnique: false });
-      }
-  
-      res.status(200).json({ isUnique: true });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Error checking email uniqueness' });
+  try {
+    const { email } = req.body;
+    console.log(req.body);
+    const existing_user = await User.findOne({ email });
+
+    if (existing_user && existing_user.isVerified) {
+      return res.status(200).json({ isUnique: false });
     }
+
+    res.status(200).json({ isUnique: true });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error checking email uniqueness" });
+  }
 };
 
-const  generateVerificationCode = () => {
-    const code_length = 4;
-    let verification_code = 0;
-    for (let i = 0; i < code_length; i++) {
-      const digit = Math.floor(Math.random() * 10);
-      verification_code = verification_code * 10 + digit;
-    }  
-    return verification_code;
+const generateVerificationCode = () => {
+  const code_length = 4;
+  let verification_code = 0;
+  for (let i = 0; i < code_length; i++) {
+    const digit = Math.floor(Math.random() * 10);
+    verification_code = verification_code * 10 + digit;
+  }
+  return verification_code;
 };
 
 const registerUser = async (req, res) => {
-    try {
-      const { first_name, last_name, email, password, birthday} = req.body;
-      const existing_user = await User.findOne({ email });
-  
-      if (existing_user) {
-        return res.status(400).json({ message: 'Email already in use' });
-      }
-  
-      const hashedPassword = await bcrypt.hash(password, 10);
-  
-      const verificationCode = generateVerificationCode();
-  
-      const user = new User({
-        first_name,
-        last_name,
-        email,
-        password: hashedPassword,
-        birthday,
-        verificationCode,
-      });
+  try {
+    const { error } = registerSchema.validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
-      const UserName = `${user.first_name} ${user.last_name}`;
-      await user.save();
+    const { first_name, last_name, email, password, birthday } = req.body;
+    const existing_user = await User.findOne({ email });
 
-      sendVerificationEmail(UserName, email, verificationCode);
-  
-      res.status(201).json({ message: 'User registered. Check your email for verification.' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Registration failed' });
+    if (existing_user) {
+      return res.status(400).json({ message: "Email already in use" });
     }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    const verificationCode = generateVerificationCode();
+
+    const user = new User({
+      first_name,
+      last_name,
+      email,
+      password: hashedPassword,
+      birthday,
+      verificationCode,
+    });
+
+    const UserName = `${user.first_name} ${user.last_name}`;
+    await user.save();
+
+    sendVerificationEmail(UserName, email, verificationCode);
+
+    res
+      .status(201)
+      .json({ message: "User registered. Check your email for verification." });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Registration failed" });
+  }
 };
 
 const verify = async (req, res) => {
-    try {
-      const { email,verificationCode } = req.body;
-      const user = await User.findOne({ email });
-  
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-  
-      if (user.verificationCode == verificationCode) {
-        user.isVerified = true;
-        user.verificationCode = null;
-        await user.save();
-  
-        const tokenPayload = {
-            _id: user._id,
-            username: `${user.first_name} ${user.last_name}`,
-            email: user.email,
-          };
-    
-        const token = jwt.sign(tokenPayload, process.env.JWT_SECRET);
-  
-        return res.status(200).json({ 
-            message: 'Email verified. You can now log in.',
-            token});
+  try {
+    const { email, verificationCode } = req.body;
+    const user = await User.findOne({ email });
 
-      } else {
-        return res.status(400).json({ message: 'Invalid verification code' });
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Verification failed' });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
+
+    if (user.verificationCode == verificationCode) {
+      user.isVerified = true;
+      user.verificationCode = null;
+      await user.save();
+
+      const token = jwt.sign({ _id: user._id }, jwtSecret);
+
+      return res.status(200).json({
+        message: "Email verified. You can now log in.",
+        token,
+      });
+    } else {
+      return res.status(400).json({ message: "Invalid verification code" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Verification failed" });
+  }
 };
 
 const login = async (req, res) => {
-    const { email, password } = req.body;
-    try {
-      const user = await User.findOne({ email });
-  
-      if (!user) {
-        return res.status(404).json({ message: 'Email is not found' });
-      }
-  
-      const is_password = await bcrypt.compare(password, user.password);
-  
-      if (!is_password) {
-        return res.status(404).json({ message: 'Password incorrect' });
-      }
+  const { email, password } = req.body;
+  try {
 
-      if(!user.isVerified){
-        return res.status(404).json({ message: 'Email not verified' });
-      }
-  
-      const tokenPayload = {
-        _id: user._id,
-        username: user.username, 
-        email: user.email,
-      };
-  
-      const token = jwt.sign(tokenPayload, process.env.JWT_SECRET);
-  
-      const { password: hashedPassword, ...userInfo } = user.toObject();
-  
-      res.status(200).json({
-        token,
-        user: userInfo,
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Internal server error' });
+    const { error } = loginSchema.validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: "Email is not found" });
     }
+
+    const is_password = await bcrypt.compare(password, user.password);
+
+    if (!is_password) {
+      return res.status(404).json({ message: "Password incorrect" });
+    }
+
+    if (!user.isVerified) {
+      return res.status(404).json({ message: "Email not verified" });
+    }
+
+    const token = jwt.sign({ _id: user._id }, jwtSecret);
+
+    const { password: hashedPassword, ...userInfo } = user.toObject();
+
+    res.status(200).json({
+      token,
+      user: userInfo,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
-
-
-module.exports = {checkEmail,registerUser,verify,login}
+module.exports = { checkEmail, registerUser, verify, login };
