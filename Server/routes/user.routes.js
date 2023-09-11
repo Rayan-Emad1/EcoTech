a91d05634a5306d9a3132e4fc9d33d13ea84ac50
Rayan <rayanemad508@gmail.com>
2023-09-12 02:04:14 +0300

    BE <> added middlewares for users route

--- Server/routes/user.routes.js
+++ Server/routes/user.routes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user.controllers");
-const { checkRole } = require("../middlewares/auth.middleware");
+const { auth, checkRole } = require("../middlewares/auth.middleware");
+
+router.get("/get_forests", auth, checkRole(['customer']), userController.getAllForestsDetails);
 
 module.exports = router;
