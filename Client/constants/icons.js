746bf9279826b14c2425de2641ec3d8284908c6c
Rayan <rayanemad508@gmail.com>
2023-09-15 14:49:13 +0300

    FE <> exported icons files

---
+++ Client/constants/icons.js
@@ -0,0 +1,21 @@
+import search from "../assets/icons/search.png";
+import left from "../assets/icons/left.png";
+import share from "../assets/icons/share.png";
+import location from "../assets/icons/location.png";
+import chevronLeft from '../assets/icons/chevron-left.png'
+import chevronRight from '../assets/icons/chevron-right.png'
+import setting from '../assets/icons/settings.png'
+import forest from '../assets/icons/forest.png'
+import statistics from '../assets/icons/statistics.png'
+
+export default {
+  search,
+  left,
+  share,
+  location,
+  chevronLeft,
+  chevronRight,
+  setting,
+  statistics,
+  forest
+};
