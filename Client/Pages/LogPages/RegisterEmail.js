b15368cf8f33c9389ff984c87b42fbe513a12e59
Rayan <rayanemad508@gmail.com>
2023-09-15 15:46:28 +0300

    FE <> initialized register email to add to stack

---
+++ Client/Pages/LogPages/RegisterEmail.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const RegisterEmail = () => {
+  return (
+    <View>
+      <Text>RegisterEmail</Text>
+    </View>
+  )
+}
+
+export default RegisterEmail
+
+const styles = StyleSheet.create({})
