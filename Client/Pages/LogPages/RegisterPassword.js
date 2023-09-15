a380ae86f5859fa8b2e9ff143883007cf8694893
Rayan <rayanemad508@gmail.com>
2023-09-15 15:46:42 +0300

    FE <> initialized register pasword to add to stack

---
+++ Client/Pages/LogPages/RegisterPassword.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const RegisterPassword = () => {
+  return (
+    <View>
+      <Text>RegisterPassword</Text>
+    </View>
+  )
+}
+
+export default RegisterPassword
+
+const styles = StyleSheet.create({})
