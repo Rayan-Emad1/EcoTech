d12d50fcdb1e1f7de7f879c80b11ea4a3b2ef5ec
Rayan <rayanemad508@gmail.com>
2023-09-15 19:50:41 +0300

    FE <> added settings stack

---
+++ Client/Pages/Main/Settings.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View, Button, SafeAreaView } from "react-native";
+import React from "react";
+
+const Settings = ({ navigation }) => {
+  return (
+    <SafeAreaView>
+      <Text>Settings</Text>
+
+
+  );
+};
+
+export default Settings;
+
+const styles = StyleSheet.create({});
