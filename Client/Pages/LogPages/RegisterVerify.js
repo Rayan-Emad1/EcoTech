74231f05ae59a85a2f343d24a7b4f29e27035e25
Rayan <rayanemad508@gmail.com>
2023-09-15 15:46:56 +0300

    FE <> initialized register verify to add to stack

---
+++ Client/Pages/LogPages/RegisterVerify.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const RegisterVerify = () => {
+  return (
+    <View>
+      <Text>RegisterVerify</Text>
+    </View>
+  )
+}
+
+export default RegisterVerify
+
+const styles = StyleSheet.create({})
