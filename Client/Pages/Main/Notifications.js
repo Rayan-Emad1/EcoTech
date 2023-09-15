10c4791212e77052d222f92731c1c17917bfe3cb
Rayan <rayanemad508@gmail.com>
2023-09-15 19:55:18 +0300

    FE <> added notification page

---
+++ Client/Pages/Main/Notifications.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View, Button , SafeAreaView } from 'react-native'
+import React from 'react'
+
+const Notifications = ({navigation}) => {
+  return (
+    <SafeAreaView>
+      <Text>Notifications</Text>
+
+    </SafeAreaView>
+  )
+}
+
+export default Notifications
+
+const styles = StyleSheet.create({})
