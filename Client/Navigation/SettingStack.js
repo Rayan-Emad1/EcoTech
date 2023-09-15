0216afd8060f00bf601264be31fa66fe43443969
Rayan <rayanemad508@gmail.com>
2023-09-15 23:52:16 +0300

    FE <> fixed stack pages name

--- Client/Navigation/SettingStack.js
+++ Client/Navigation/SettingStack.js
@@ -1,15 +1,19 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import Profile from "./Main/Profile";
-import Settings from "./Main/Settings";
+import Profile from "../Pages/Main/Profile";
+import Settings from "../Pages/Main/Settings";
 
 const Stack = createStackNavigator();
 
 const SettingsStack = () => {
   return (
     <Stack.Navigator initialRouteName="Settings">
-      <Stack.Screen name="Settings" component={Settings} options={{ headerShown: false ,  gestureEnabled: false }} />
+      <Stack.Screen
+        name="Settings"
+        component={Settings}
+        options={{ headerShown: false, gestureEnabled: false }}
+      />
       <Stack.Screen name="Profile" component={Profile} />
     </Stack.Navigator>
   );
