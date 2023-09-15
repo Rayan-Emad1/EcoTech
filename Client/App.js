3baeaa279e7da7ada543033d2a521293cd950678
Rayan <rayanemad508@gmail.com>
2023-09-15 19:44:04 +0300

    FE <> added stack to app

--- Client/App.js
+++ Client/App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <StackPages />
+        <StackPages/>
       </NavigationContainer>
     </SafeAreaProvider>
   );
