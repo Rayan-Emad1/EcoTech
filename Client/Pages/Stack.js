ef50aaa97d6cb0eabb2dfba6a59ba74a9aa2fb5b
Rayan <rayanemad508@gmail.com>
2023-09-15 16:40:57 +0300

    FE <> added login page to stack

--- Client/Pages/Stack.js
+++ Client/Pages/Stack.js
@@ -1,12 +1,24 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
+import Login from "./LogPages/Login";
+import RegisterCredentials from "./LogPages/RegisterCredentials";
+import RegisterEmail from "./LogPages/RegisterEmail";
+import RegisterPassword from "./LogPages/RegisterPassword";
+import RegisterVerify from "./LogPages/RegisterVerify";
+import TabPages from "./Tab";
 
 const Stack = createStackNavigator();
 
 const StackPages = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        header: () => null,
+      }}
+      initialRouteName="Home"
+    >
+      <Stack.Screen name="Login" component={Login} />
 
     </Stack.Navigator>
   );
