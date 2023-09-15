5d11fb623e86a73c8f08f72a74f708d74c648afa
Rayan <rayanemad508@gmail.com>
2023-09-15 19:47:14 +0300

    FE <> added statistics tab

---
+++ Client/Pages/Main/Statistics.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, SafeAreaView, View } from 'react-native'
+import React from 'react'
+
+const Statistics = ({navigation}) => {
+  return (
+    <SafeAreaView>
+      <Text>Statistics</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Statistics
+
+const styles = StyleSheet.create({})
