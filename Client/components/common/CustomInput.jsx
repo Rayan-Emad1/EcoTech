0941799d05a8906982a6d837c53af7f237f848c7
Rayan <rayanemad508@gmail.com>
2023-09-16 12:47:35 +0300

    FE <> changed components folder

--- Client/components/common/CustomInput.jsx
+++ Client/components/common/CustomInput.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, TextInput, Text, StyleSheet } from "react-native";
-import { COLORS, SIZES } from "../../../constants/index";
+import { COLORS, SIZES } from "../../constants/index";
 
 const CustomInput = ({ placeholder, title, value, onChangeText, secureTextEntry }) => {
   return (
