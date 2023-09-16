6e098ef32dc57572085751296df2afb1be30d6e0
Rayan <rayanemad508@gmail.com>
2023-09-16 23:01:32 +0300

    FE <> added back button to verify page

--- Client/Pages/LogPages/RegisterVerify.js
+++ Client/Pages/LogPages/RegisterVerify.js
@@ -1,18 +1,17 @@
-import { Button, StyleSheet, Text, View , SafeAreaView} from 'react-native'
-import React from 'react'
+import { Button, StyleSheet, Text, View, SafeAreaView } from "react-native";
+import React from "react";
+import SubmitButton from "../../components/common/SubmitButton";
+import BackButton from "../../components/common/BackButton";
+import CustomTitle from "../../components/common/CustomTitle";
 
-const RegisterVerify = ({navigation}) => {
+const RegisterVerify = ({ navigation }) => {
   return (
     <SafeAreaView>
-      <Text>RegisterVerify</Text>
-      <Button
-        title="Go to Main Page Screen"
-        onPress={() => navigation.replace("Main")}
-      />
+      <BackButton navigation={navigation} />
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default RegisterVerify
+export default RegisterVerify;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
