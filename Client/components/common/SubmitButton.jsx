719babae5db16a121e0b450d55e917563524e323
Rayan <rayanemad508@gmail.com>
2023-09-16 12:49:26 +0300

    FE <> added submit button component

---
+++ Client/components/common/SubmitButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+import { COLORS } from "./theme";
+
+const SubmitButton = ({ text, onPress, disabled , set_color }) => {
+  return (
+    <TouchableOpacity
+
+    >
+    </TouchableOpacity>
+  );
+};
+
+export default SubmitButton;
