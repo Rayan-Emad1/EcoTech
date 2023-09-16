f97020431e4cb77d840fa4d5421b48ad3a6c13df
Rayan <rayanemad508@gmail.com>
2023-09-16 11:11:44 +0300

    FE <> added custom input component

---
+++ Client/components/common/input/CustomInput.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, TextInput, Text, StyleSheet } from "react-native";
+import { COLORS, SIZES } from "../../../constants/index";
+
+const CustomInput = ({ placeholder, title, value, onChangeText, secureTextEntry }) => {
+  return (
+    <View style={styles.inputContainer}>
+
+    </View>
+  );
+};
+
+
+
+export default CustomInput;
