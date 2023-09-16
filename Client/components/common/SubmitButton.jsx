8bb343be3e1d09b5ea3b1bb08c42dcff6c68e609
Rayan <rayanemad508@gmail.com>
2023-09-16 12:50:27 +0300

    FE <> added submit button props

--- Client/components/common/SubmitButton.jsx
+++ Client/components/common/SubmitButton.jsx
@@ -5,8 +5,14 @@ import { COLORS } from "./theme";
 const SubmitButton = ({ text, onPress, disabled , set_color }) => {
   return (
     <TouchableOpacity
-
+      style={[
+        styles.button,
+        { backgroundColor: set_color == "green" ? COLORS.green : COLORS.red },
+      ]}
+      onPress={onPress}
+      disabled={disabled}
     >
+      <Text style={styles.buttonText}>{text}</Text>
     </TouchableOpacity>
   );
 };
