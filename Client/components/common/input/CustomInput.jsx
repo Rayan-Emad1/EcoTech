64a4e871e1638eefd1db126476b0f3b0309b4dbc
Rayan <rayanemad508@gmail.com>
2023-09-16 11:13:31 +0300

    FE <> added text input to custom input

--- Client/components/common/input/CustomInput.jsx
+++ Client/components/common/input/CustomInput.jsx
@@ -6,7 +6,13 @@ const CustomInput = ({ placeholder, title, value, onChangeText, secureTextEntry 
   return (
     <View style={styles.inputContainer}>
       <Text style={styles.inputTitle}>{title}</Text>
-
+      <TextInput
+        style={styles.input}
+        placeholder={placeholder}
+        value={value}
+        onChangeText={onChangeText}
+        secureTextEntry={secureTextEntry}
+      />
     </View>
   );
 };
