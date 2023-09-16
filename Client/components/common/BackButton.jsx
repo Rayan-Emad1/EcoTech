ef95c2a810153bf087ff754791947ebdb6103c06
Rayan <rayanemad508@gmail.com>
2023-09-16 17:36:19 +0300

    FE <> added back button component

---
+++ Client/components/common/BackButton.jsx
@@ -0,0 +1,18 @@
+import {
+    StyleSheet,
+    Image,
+    TouchableOpacity,
+  } from "react-native";
+import React from 'react'
+import { icons } from "../../constants/index";
+
+const BackButton = ({navigation}) => {
+  return (
+
+  )
+}
+
+export default BackButton
+
+const styles = StyleSheet.create({
+})
