b74dc783f2ad84c70ce810995d73b2c22f5d7585
Rayan <rayanemad508@gmail.com>
2023-09-16 23:11:09 +0300

    FE <> added container style to verify page

--- Client/Pages/LogPages/RegisterVerify.js
+++ Client/Pages/LogPages/RegisterVerify.js
@@ -34,7 +34,12 @@ const RegisterVerify = ({ navigation }) => {
 export default RegisterVerify;
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
 
 
 });
