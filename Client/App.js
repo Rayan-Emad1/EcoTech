541f14d0f31709eaaefa3a351a9275f3aa168757
Rayan <rayanemad508@gmail.com>
2023-09-16 17:02:25 +0300

    FE <> added disable keyboard when screen touched

--- Client/App.js
+++ Client/App.js
@@ -2,13 +2,16 @@ import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import StackPages from "./Navigation/StackPages";
+import { Keyboard, TouchableWithoutFeedback } from "react-native";
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <StackPages/>
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <StackPages />
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </TouchableWithoutFeedback>
   );
 }
