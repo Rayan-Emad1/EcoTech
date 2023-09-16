921043c5eb7d7b33be59f12b418b763015ea2b7c
Rayan <rayanemad508@gmail.com>
2023-09-16 22:27:16 +0300

    FE <> added custom title component

---
+++ Client/components/common/CustomTitle.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { COLORS, SIZES } from '../../constants/index';
+
+const CustomTitle = ({ title, subtitle }) => {
+  return (
+    <View style={styles.title_container}>
+
+
+    </View>
+  );
+};
+
+export default CustomTitle;
+
+const styles = StyleSheet.create({
+
+
+
+});
