167a11a43fcd8972f758389fbcab30f269254390
Rayan <rayanemad508@gmail.com>
2023-09-16 23:10:23 +0300

    FE <> added otp message

--- Client/Pages/LogPages/RegisterVerify.js
+++ Client/Pages/LogPages/RegisterVerify.js
@@ -1,22 +1,40 @@
-import { Button, StyleSheet, Text, View, SafeAreaView } from "react-native";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Alert,
+} from "react-native";
 import React from "react";
+import { COLORS, SIZES, images } from "../../constants/index";
 import SubmitButton from "../../components/common/SubmitButton";
 import BackButton from "../../components/common/BackButton";
 import CustomTitle from "../../components/common/CustomTitle";
 
 const RegisterVerify = ({ navigation }) => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <BackButton navigation={navigation} />
       <CustomTitle
         title="Verify OTP"
         subtitle="Please enter the code we sent you to email"
       />
       {/* <OTP/> */}
+      <View style={styles.OTP_message}>
+        <Text style={styles.OTP_message}>Did not Receive OTP ?</Text>
+        <Text style={styles.OTP_link} onPress={() => <Alert title="hello" />}>
+          Resend Code
+        </Text>
+      </View>
     </SafeAreaView>
   );
 };
 
 export default RegisterVerify;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+
+
+
+});
