0c5ee6494b4698ff83181ede171e5c033027a80d
Rayan <rayanemad508@gmail.com>
2023-09-16 23:14:10 +0300

    FE <> added progress element styles in verify page

--- Client/Pages/LogPages/RegisterVerify.js
+++ Client/Pages/LogPages/RegisterVerify.js
@@ -56,4 +56,25 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   
+  progressContainer: {
+    flexDirection: "row",
+    width: "75%",
+    backgroundColor: "grey",
+    borderRadius: 55,
+  },
+  complete: {
+    flex: 1,
+    height: 8,
+    backgroundColor: COLORS.green,
+    borderRadius: 55,
+    maxWidth: "66.66%",
+  },
+  progressText: {
+    position: "relative",
+    left: "30%",
+    marginBottom: 10,
+    marginLeft: SIZES.medium,
+    fontSize: SIZES.small,
+    color: COLORS.black,
+  },
 });
