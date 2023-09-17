289ab26871c779a009e31606a729fae5c984cbee
Rayan <rayanemad508@gmail.com>
2023-09-17 10:59:19 +0300

    FE <> added initial region on map

--- Client/Pages/Main/Map.js
+++ Client/Pages/Main/Map.js
@@ -1,27 +1,39 @@
-import React from 'react'
-import { StyleSheet, Text, View ,SafeAreaView } from 'react-native'
-import MapView from 'react-native-maps';
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import MapView from "react-native-maps";
 
 const Map = () => {
+  const handleChange = (place) => {
+    console.log(place);
+  };
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} ></MapView>
+      <MapView
+        style={styles.map}
+        onRegionChange={handleChange}
+        initialRegion={{
+          latitude: 33.83728746204912,
+          latitudeDelta: 2.1746411420983733,
+          longitude: 35.91056445540316,
+          longitudeDelta: 1.4095237243800227,
+        }}
+      ></MapView>
     </View>
-  )
-}
+  );
+};
 
-export default Map
+export default Map;
 
 const styles = StyleSheet.create({
-
   container: {
-    flex:1,
-    backgroundColor:'transparent',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flex: 1,
+    backgroundColor: "transparent",
+    justifyContent: "center",
+    alignItems: "center",
   },
-  map:{
-    width:"100%",
-    height:"100%",
-  }
-})
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
