65ad01ccbe8d399d25911fa5decdf96e7721185c
Rayan <rayanemad508@gmail.com>
2023-09-18 22:20:13 +0300

    FE <> exported weather widget images

--- Client/constants/images.js
+++ Client/constants/images.js
@@ -1,9 +1,13 @@
 import profile from "../assets/images/profile.jpg";
 import leaves from "../assets/images/leaves.png";
-import safe_pin from "../assets/images/safe_forest_pin2.png"
-import dangerous_pin from "../assets/images/dangerous_forest_pin2.png"
-import safe_card from '../assets/images/forest_safe.png'
-import danger_card from '../assets/images/forest_dangerous.png'
+import safe_pin from "../assets/images/safe_forest_pin2.png";
+import dangerous_pin from "../assets/images/dangerous_forest_pin2.png";
+import safe_card from "../assets/images/forest_safe.png";
+import danger_card from "../assets/images/forest_dangerous.png";
+import sunny from "../assets/images/sunny.png";
+import windy from "../assets/images/windy.png";
+import stormy from "../assets/images/stormy.png";
+import night from "../assets/images/night.png";
 
 export default {
   profile,
@@ -11,5 +15,9 @@ export default {
   danger_card,
   leaves,
   safe_pin,
-  dangerous_pin
+  dangerous_pin,
+  sunny,
+  windy,
+  stormy,
+  night,
 };
