6ec031fe93fb06f8c1e314cf366277c3b367bb62
Rayan <rayanemad508@gmail.com>
2023-09-18 19:58:39 +0300

    FE <> added props to custom header

--- Client/components/common/CustomHeader.jsx
+++ Client/components/common/CustomHeader.jsx
@@ -1,46 +1,43 @@
-import {StyleSheet,View,Image,Platform,TextInput,Pressable} from "react-native";
-import React, { useState} from "react";
+import React, { useState } from "react";
+import { StyleSheet, View, Image, Platform, TextInput, Pressable } from "react-native";
 import { COLORS, icons } from "../../constants/index";
 
+const CustomHeader = ({setSearchValue }) => {
+  const [isSearchVisible, setIsSearchVisible] = useState(false);
+  const toggleSearch = () => {
+    setIsSearchVisible(!isSearchVisible);
+  };
 
-
-const CustomHeader = () => {
-
-    const [isSearchVisible, setIsSearchVisible] = useState(false);
-
-    const toggleSearch = () => {
-      setIsSearchVisible(!isSearchVisible);
-    };
-  
   return (
- <View style={styles.top}>
-    <Pressable style={styles.Icon_background}>
-      <Image source={icons.profile} />
-    </Pressable>
-    <View>
-      {isSearchVisible ? (
-        <View style={styles.searchContainer}>
-          <TextInput
-            placeholder="Search here"
-            placeholderTextColor={COLORS.white}
-            autoCapitalize="none"
-            style={styles.searchBox}
-          />
-          <Pressable onPress={toggleSearch}>
+    <View style={styles.top}>
+      <Pressable style={styles.Icon_background}>
+        <Image source={icons.profile} />
+      </Pressable>
+      <View>
+        {isSearchVisible ? (
+          <View style={styles.searchContainer}>
+            <TextInput
+              placeholder="Search here"
+              placeholderTextColor={COLORS.white}
+              autoCapitalize="none"
+              style={styles.searchBox}
+              onChangeText={(text) => setSearchValue(text)}
+            />
+            <Pressable onPress={toggleSearch}>
+              <Image source={icons.search} />
+            </Pressable>
+          </View>
+        ) : (
+          <Pressable onPress={toggleSearch} style={styles.Icon_background}>
             <Image source={icons.search} />
           </Pressable>
-        </View>
-      ) : (
-        <Pressable onPress={toggleSearch} style={styles.Icon_background}>
-          <Image source={icons.search} />
-        </Pressable>
-      )}
+        )}
+      </View>
     </View>
-  </View>
-  )
-}
+  );
+};
 
-export default CustomHeader
+export default CustomHeader;
 
 const styles = StyleSheet.create({
   top: {
