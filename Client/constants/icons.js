3e972d3e16f7b2736a948e83b80fb91869041db4
Rayan <rayanemad508@gmail.com>
2023-09-19 00:54:27 +0300

    FE <> fixed imports

--- Client/constants/icons.js
+++ Client/constants/icons.js
@@ -3,17 +3,19 @@ import search from "../assets/icons/search.png";
 import profile from "../assets/icons/profile.png";
 import share from "../assets/icons/share.png";
 import location from "../assets/icons/location.png";
-import chevronLeft from '../assets/icons/chevron-left.png'
-import chevronRight from '../assets/icons/chevron-right.png'
-import setting from '../assets/icons/settings.png'
-import forest from '../assets/icons/forest.png'
-import statistics from '../assets/icons/statistics.png'
-import safe from '../assets/icons/safe-icon.png'
-import danger from '../assets/icons/danger-icon.png'
-import humidity from '../assets/icons/humidity.png'
-import wind from '../assets/icons/wind.png'
-import temperature from '../assets/icons/temperature.png'
-
+import chevronLeft from "../assets/icons/chevron-left.png";
+import chevronRight from "../assets/icons/chevron-right.png";
+import setting from "../assets/icons/settings.png";
+import forest from "../assets/icons/forest.png";
+import statistics from "../assets/icons/statistics.png";
+import safe from "../assets/icons/safe-icon.png";
+import danger from "../assets/icons/danger-icon.png";
+import humidity from "../assets/icons/humidity.png";
+import wind from "../assets/icons/wind.png";
+import temperature from "../assets/icons/temperature.png";
+import google from "../assets/icons/google.png";
+import apple from "../assets/icons/apple.png";
+import facebook from "../assets/icons/facebook.png";
 
 export default {
   safe,
@@ -30,5 +32,8 @@ export default {
   chevronRight,
   setting,
   statistics,
-  forest
+  forest,
+  google,
+  apple,
+  facebook,
 };
