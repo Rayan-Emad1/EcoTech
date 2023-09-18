f8d3fad597d2ca8288281d168baf35e41fb5e887
Rayan <rayanemad508@gmail.com>
2023-09-18 21:35:39 +0300

    FE <> added forest card

---
+++ Client/components/Map/ForestCard.jsx
@@ -0,0 +1,30 @@
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
+  } from "react-native";
+  import React from "react";
+  
+  import { COLORS, SIZES, images, icons } from "../../constants/index";
+
+const ForestCard = () => {
+  return (
+    <View style={styles.forest_container}>
+
+
+  </View>
+  )
+}
+
+export default ForestCard
+
+const styles = StyleSheet.create({
+
+
+
+
+
+
+
+  });
