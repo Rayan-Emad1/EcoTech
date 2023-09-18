ebfa0074fc38abc4330dddabe2fcce2c0a2ce74d
Rayan <rayanemad508@gmail.com>
2023-09-18 10:57:29 +0300

    FE <> fixed image extension

--- Client/constants/images.js
+++ Client/constants/images.js
@@ -1,7 +1,7 @@
 import profile from "../assets/images/profile.jpg";
 import leaves from "../assets/images/leaves.png";
-import safe_pin from "../assets/images/safe_forest_pin"
-import dangerous_pin from "../assets/images/dangerous_forest_pin"
+import safe_pin from "../assets/images/safe_forest_pin.png"
+import dangerous_pin from "../assets/images/dangerous_forest_pin.png"
 
 export default {
   profile,
