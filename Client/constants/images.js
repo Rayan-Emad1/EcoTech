961cb5d8bb04ae6fb65c048ec8dcebdbf45ec03d
Rayan <rayanemad508@gmail.com>
2023-09-18 13:38:31 +0300

    FE <> exported images

--- Client/constants/images.js
+++ Client/constants/images.js
@@ -1,7 +1,7 @@
 import profile from "../assets/images/profile.jpg";
 import leaves from "../assets/images/leaves.png";
-import safe_pin from "../assets/images/safe_forest_pin.png"
-import dangerous_pin from "../assets/images/dangerous_forest_pin.png"
+import safe_pin from "../assets/images/safe_forest_pin2.png"
+import dangerous_pin from "../assets/images/dangerous_forest_pin2.png"
 
 export default {
   profile,
