88102608ac6f821c2b3ede5f0ed16d079a4a3552
Rayan <rayanemad508@gmail.com>
2023-09-19 19:27:09 +0300

    FE <> imported components to profile page

--- Client/Pages/Main/Profile.js
+++ Client/Pages/Main/Profile.js
@@ -1,14 +1,43 @@
-import { StyleSheet, Text, View , SafeAreaView } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
+import React, { useState } from "react";
+import BackButton from "../../components/common/BackButton";
+import { SIZES, COLORS, images, icons } from "../../constants";
+import SubmitButton from "../../components/common/SubmitButton";
+import CustomInput from "../../components/common/CustomInput";
 
-const Profile = ({navigation}) => {
+const Profile = ({ navigation }) => {
+  const [Name, setName] = useState("Daniel Yehya");
+  const [email, setEmail] = useState("daniel@gmail.com");
+
   return (
-    <SafeAreaView>
-      <Text>Profile</Text>
+    <SafeAreaView style={styles.container}>
+      <BackButton navigation={navigation} />
+
+
+
+
+
+
+
+
+
+
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginVertical: 20,
+  },
+
+
+
+
+});
