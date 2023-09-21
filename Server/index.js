bb385f0f5e386037856578dc2435dbd15442726b
Rayan <rayanemad508@gmail.com>
2023-09-21 23:55:20 +0300

    BE <> changed port number to run on server

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 80;
 const mongooseConnect = require("./configs/mongoDB.connect");
 const rateLimit = require("express-rate-limit");
 
