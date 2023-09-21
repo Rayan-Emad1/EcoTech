238e4601e816cb7bc000533da84a77824e3e5b70
Rayan <rayanemad508@gmail.com>
2023-09-21 23:06:33 +0300

    FE <> added custom icons to tab bar

--- Client/Navigation/TabPages.js
+++ Client/Navigation/TabPages.js
@@ -1,18 +1,63 @@
 import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { COLORS, SIZES, images, icons } from "../constants/index";
+import { Image } from "react-native";
+
 import Map from "../Pages/Main/Map";
 import NotificationStack from "./NotificationStack";
 import SettingsStack from "./SettingStack";
-
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const Tab = createBottomTabNavigator();
 
 const TabPages = () => {
   return (
-    <Tab.Navigator initialRouteName="Map" screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="Map" component={Map} />
-      <Tab.Screen name="NotificationStack" component={NotificationStack} />
-      <Tab.Screen name="SettingsStack" component={SettingsStack} />
+    <Tab.Navigator
+      initialRouteName="NotificationStack"
+      screenOptions={{ headerShown: false }}
+      tabBarOptions={{
+        showLabel: false,
+      }}
+      tabBarStyle={{
+        backgroundColor: COLORS.black,
+        borderTopWidth: 0,
+      }}
+    >
+      <Tab.Screen
+        name="Map"
+        component={Map}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Image
+              source={icons.forest}
+              style={{ tintColor: color, width: size, height: size }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="NotificationStack"
+        component={NotificationStack}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Image
+              source={icons.statistics}
+              style={{ tintColor: color, width: size, height: size }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="SettingsStack"
+        component={SettingsStack}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Image
+              source={icons.setting}
+              style={{ tintColor: color, width: size, height: size }}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
