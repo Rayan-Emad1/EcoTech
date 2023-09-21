eb33be64a6650dedd2b504c9500a1751e440c2e4
Rayan <rayanemad508@gmail.com>
2023-09-21 23:15:51 +0300

    FE <> created temperature hourly static data

--- Client/Pages/Main/data.js
+++ Client/Pages/Main/data.js
@@ -35,6 +35,35 @@ const WeeklyHumid = {
   ],
 };
 
+const HourlyTemp = {
+  labels: [
+    "1:00",
+    "2:00",
+    "3:00",
+    "4:00",
+    "5:00",
+    "6:00",
+    "7:00",
+    "8:00",
+    "9:00",
+    "10:00",
+    "11:00",
+    "12:00",
+  ],
+  datasets: [
+    {
+      data: [24, 23, 22, 21, 21, 24, 23, 22, 21, 21, 21, 30, 24],
+      color: (opacity = 0.1) => `rgba(0, 250, 0, ${opacity})`,
+      legend: ["Predicted Values"],
+    },
+    {
+      data: [24, 23, 22, 21, 21, 24, 23, 22, 21, 21],
+      color: (opacity = 1) => `rgba(244, 244, 244, ${opacity})`,
+      legend: ["Real Values"],
+    },
+
+  ],
+};
 
 
 
