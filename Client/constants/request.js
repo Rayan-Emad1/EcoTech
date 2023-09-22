3b14535cbbc887e94f239562697854e79dad8f9e
Rayan <rayanemad508@gmail.com>
2023-09-22 14:39:52 +0300

    FE <> added login request

--- Client/constants/request.js
+++ Client/constants/request.js
@@ -2,4 +2,16 @@ import axios from "axios";
 
 const BASE_URL = "http://192.168.0.5:8080/";
 
+const login = async (email, password) => {
+  try {
+    const response = await axios.post(`${BASE_URL}auth/login`, {
+      email,
+      password,
+    });
+    return response.data;
+  } catch (error) {
+    throw error.response ? error.response.data : error.message;
+  }
+};
 
+export { login };
