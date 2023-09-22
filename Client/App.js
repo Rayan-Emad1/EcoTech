21145265f48f51e9a382759f14221664a5372bd5
Rayan <rayanemad508@gmail.com>
2023-09-22 14:24:37 +0300

    FE <> added axios package

--- Client/App.js
+++ Client/App.js
@@ -12,6 +12,6 @@ export default function App() {
           <StackPages />
         </NavigationContainer>
       </SafeAreaProvider>
-    // {/* </TouchableWithoutFeedback>  */}
+    // </TouchableWithoutFeedback>  
   );
 }
