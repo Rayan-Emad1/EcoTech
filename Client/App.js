78597b37b18a053cb58f498b0a261979a9e5a16b
Rayan <rayanemad508@gmail.com>
2023-09-22 15:32:37 +0300

    FE <> wrapped app with provider

--- Client/App.js
+++ Client/App.js
@@ -2,16 +2,20 @@ import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import StackPages from "./Navigation/StackPages";
-import { Keyboard, TouchableWithoutFeedback } from "react-native";
+import { Provider } from "react-redux";
+import store from "./Redux/store";
+
+// <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+// </TouchableWithoutFeedback>
 
 export default function App() {
   return (
-    // <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+    <Provider store={store}>
       <SafeAreaProvider>
         <NavigationContainer>
           <StackPages />
         </NavigationContainer>
       </SafeAreaProvider>
-    // </TouchableWithoutFeedback>  
+    </Provider>
   );
 }
