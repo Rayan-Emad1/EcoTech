e7937c83abc3e652f72a8e855319d0885092a87e
Rayan <rayanemad508@gmail.com>
2023-09-23 11:32:08 +0300

    BE <> modified user model

--- Server/models/users.model.js
+++ Server/models/users.model.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   last_name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  birthday: { type: Date, default: null },
+  birthday: { type: String, default: null },
   address: { type: String, default: null },
   notification_token: { type: String, default: null },
   verification_code: { type: String, default: null },
