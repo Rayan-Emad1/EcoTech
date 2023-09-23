98c79905e0b24f4ee3ab8a601156c3e3e6fe3ec7
Rayan <rayanemad508@gmail.com>
2023-09-23 11:50:35 +0300

    FE <> added check email axios

--- Client/constants/request.js
+++ Client/constants/request.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://192.168.0.5:8080/";
+const BASE_URL = "http://192.168.0.2:8080/";
 
 const login = async (email, password) => {
   try {
@@ -14,4 +14,17 @@ const login = async (email, password) => {
   }
 };
 
-export { login };
+const checkEmail = async (email) => {
+  try {
+    const response = await axios.post(`${BASE_URL}auth/check_email`, {
+      email,
+    });
+    return response.data;
+  } catch (error) {
+    throw error.response ? error.response.data : error.message;
+  }
+};
+
+
+
+export { login, checkEmail };
