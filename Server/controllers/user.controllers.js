b8ce8dbf4fdae48c9db649099bcd8e8462e91937
Rayan <rayanemad508@gmail.com>
2023-09-23 14:53:14 +0300

    BE <> modified user controller

--- Server/controllers/user.controllers.js
+++ Server/controllers/user.controllers.js
@@ -6,22 +6,24 @@ const getAllForestsDetails = async (_, res) => {
     const forests = await Forest.find(
       {},
       {
+        id:1,
         name: 1,
         location: 1,
         description: 1,
         current_temperature: 1,
         current_humidity: 1,
-        current_wind: 1,
+        // current_wind: 1,
         fire_alarm: 1,
       }
     );
 
     const formattedForests = forests.map((forest) => ({
+      id:forest.id,
       name: forest.name,
       description: forest.description,
       current_temperature: forest.current_temperature.value,
       current_humidity: forest.current_humidity.value,
-      current_wind: forest.current_wind.value,
+      // current_wind: forest.current_wind.value,
       last_update: forest.current_temperature.timestamp,
       fire_alarm: forest.fire_alarm,
       location: forest.location,
