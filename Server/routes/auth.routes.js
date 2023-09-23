32a1cd58ae585c877ad585e352da132490aa2b1a
Rayan <rayanemad508@gmail.com>
2023-09-23 18:41:09 +0300

    BE <> added update route with middleware

--- Server/routes/auth.routes.js
+++ Server/routes/auth.routes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth.controllers");
+const { auth } = require("../middlewares/auth.middleware");
 
 router.post("/check_email", authController.checkEmail);
 router.post("/register_user", authController.registerUser);
 router.post("/verify", authController.verify);
 router.post("/login", authController.login);
+router.post("/update", auth, authController.updateUser);
 
 module.exports = router;
