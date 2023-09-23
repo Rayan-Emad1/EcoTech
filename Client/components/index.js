a336dfc2a061c52ccf2ca7aba13ce1aac505c280
Rayan <rayanemad508@gmail.com>
2023-09-23 11:28:52 +0300

    FE <> added index in components for clean imports

---
+++ Client/components/index.js
@@ -0,0 +1,17 @@
+import SubmitButton from "./common/SubmitButton";
+import BackButton from "./common/BackButton";
+import CustomTitle from "./common/CustomTitle";
+import CustomInput from "./common/CustomInput";
+import Dropdown from "./Notification/Dropdown";
+import WeatherWidget from "./Notification/WeatherWidget";
+import ForestCard from "./Map/ForestCard";
+
+export {
+  SubmitButton,
+  BackButton,
+  CustomTitle,
+  CustomInput,
+  Dropdown,
+  WeatherWidget,
+  ForestCard,
+};
