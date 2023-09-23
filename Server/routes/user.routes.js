d263a9104211e282ccc6a49c7afece17d7e6eec3
Rayan <rayanemad508@gmail.com>
2023-09-23 20:01:35 +0300

    BE <> fixed bug in middleware

--- Server/routes/user.routes.js
+++ Server/routes/user.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require("../controllers/user.controllers");
 const { auth, checkRole } = require("../middlewares/auth.middleware");
 
-router.get("/get_forests", auth, checkRole(['customer']), userController.getAllForestsDetails);
+router.get("/get_forests", auth, checkRole(['customer','admin']), userController.getAllForestsDetails);
 router.post("/get_forest_data", userController.getForestData);
 
 module.exports = router;
