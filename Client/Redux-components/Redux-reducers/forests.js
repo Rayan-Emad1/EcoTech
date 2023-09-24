c24e32a62b7161d6a5207d4eccd7102ea12d38e6
Rayan <rayanemad508@gmail.com>
2023-09-24 12:02:17 +0300

    FE <> modified reducer of forests

--- Client/Redux-components/Redux-reducers/forests.js
+++ Client/Redux-components/Redux-reducers/forests.js
@@ -5,7 +5,7 @@ const INITIAL_STATE = {
   export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
       case 'SET_FORESTS':
-        return { ...state, forests: action.payload };
+        return { ...action.payload };
       default:
         return state;
     }
