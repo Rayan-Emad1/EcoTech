4954bfb3666faea50590397247991441b78f3988
Rayan <rayanemad508@gmail.com>
2023-09-24 15:06:58 +0300

    FE <> added get forest data axios with data set

--- Client/constants/request.js
+++ Client/constants/request.js
@@ -95,4 +95,129 @@ const getForests = async () => {
   }
 };
 
-export { login, checkEmail, registerUser, verify, updateProfile, getForests };
+const fetchAndTransformForestData = async (forestId) => {
+  try {
+    const token = await AsyncStorage.getItem("token");
+    const headers = {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    };
+    const response = await axios.post(
+      `${BASE_URL}user/get_forest_data`,
+      { forestId: forestId },
+      { headers }
+    );
+    const forestData = response.data;
+
+    // Determine the color based on fire_alarm
+    const colorForFireAlarm = forestData.fire_alarm
+      ? "rgba(255, 0, 0, 1)"
+      : "rgba(0, 255, 0, 1)";
+
+    // Filter and map real values for hourly temperature
+    const hourlyRealTemperatureData = forestData.todaysData.temperature
+      .filter((data) => data.source === "real")
+      .map((data) => data.value);
+
+    const hourlyTempData = {
+      labels: forestData.todaysData.temperature
+        .filter((data) => data.source === "real")
+        .map((data) => `${data.hour}:00`),
+      datasets: [
+        {
+          data: hourlyRealTemperatureData,
+          color: (opacity = 1) => colorForFireAlarm,
+          legend: ["Predicted Values"],
+        },
+        {
+          data: hourlyRealTemperatureData,
+          color: (opacity = 1) => "rgba(255, 255, 255, " + opacity + ")",
+          legend: ["Real Values"],
+        },
+      ],
+    };
+
+    // Filter and map real values for weekly temperature
+    const weeklyRealTemperatureData =
+      forestData.threeWeeksOfData.currentWeek.temperature
+        .filter((data) => data.source === "real")
+        .map((data) => data.value);
+
+    const weeklyTempData = {
+      labels: forestData.threeWeeksOfData.currentWeek.temperature
+        .filter((data) => data.source === "real")
+        .map((data) => `${data.day}/${data.month}`),
+      datasets: [
+        {
+          data: weeklyRealTemperatureData,
+          color: (opacity = 1) => colorForFireAlarm,
+          legend: ["Predicted Values"],
+        },
+        {
+          data: weeklyRealTemperatureData,
+          color: (opacity = 1) => "rgba(244, 244, 244, " + opacity + ")",
+          legend: ["Real Values"],
+        },
+      ],
+    };
+
+    // Filter and map real values for hourly humidity (similar changes can be made for other datasets)
+
+    const hourlyRealHumidityData = forestData.todaysData.humidity
+      .filter((data) => data.source === "real")
+      .map((data) => data.value);
+
+    const hourlyHumidData = {
+      labels: forestData.todaysData.humidity
+        .filter((data) => data.source === "real")
+        .map((data) => `${data.hour}:00`),
+      datasets: [
+        {
+          data: hourlyRealHumidityData,
+          color: (opacity = 1) => "rgba(244, 244, 244, " + opacity + ")",
+          legend: ["Real Values"],
+        },
+      ],
+    };
+
+    // Filter and map real values for weekly humidity (similar changes can be made for other datasets)
+
+    const weeklyRealHumidityData =
+      forestData.threeWeeksOfData.currentWeek.humidity
+        .filter((data) => data.source === "real")
+        .map((data) => data.value);
+
+    const weeklyHumidData = {
+      labels: forestData.threeWeeksOfData.currentWeek.humidity
+        .filter((data) => data.source === "real")
+        .map((data) => `${data.day}/${data.month}`),
+      datasets: [
+        {
+          data: weeklyRealHumidityData,
+          color: (opacity = 1) => "rgba(244, 244, 244, " + opacity + ")",
+          legend: ["Real Values"],
+        },
+      ],
+    };
+
+    return {
+      hourlyTempData,
+      weeklyTempData,
+      hourlyHumidData,
+      weeklyHumidData,
+    };
+  } catch (error) {
+    console.error("E", error);
+    throw error;
+  }
+};
+
+export {
+  login,
+  checkEmail,
+  registerUser,
+  verify,
+  updateProfile,
+  getForests,
+  fetchAndTransformForestData,
+};
