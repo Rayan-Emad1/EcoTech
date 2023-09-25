0ad3177396d54edf866e737b2addca86f02cf165
Rayan <rayanemad508@gmail.com>
2023-09-25 22:04:47 +0300

    AI <> added prompt to get proper response

--- Server/configs/open.ai.js
+++ Server/configs/open.ai.js
@@ -11,6 +11,49 @@ const openai = new OpenAI({
 
 // Prediction function
 const OpenAiPrediction = async (input) => {
+  // Define the modified prompt
+  const prompt = `
+I want you to act as an AI assistant that can analyze forest sensor data and generate a forecast.
+
+Imagine you are helping a forest researcher analyze data from a weather monitoring station in a forest.
+
+The data includes a forest ID, temperature readings, and humidity readings. The temperature and humidity readings are coming from a sensor with the time and day (24-hour format).
+
+Your task is to provide a detailed forecast based on this data and the current condition in the forest, considering the forest's coordinates.
+
+Here's the input data format:
+
+${JSON.stringify(input, null, 2)}
+
+Please generate a JSON object, taking the following as an example with the same structure as provided:
+
+{
+  "forestId": "650feb23594bb545d4cdd338",
+  "temperature": [
+    { "value": 17.5, "hour": 24, "day": 24, "source": "real" },
+    { "value": 18, "hour": 1, "day": 25, "source": "predicted" }
+    { "value": 18.5, "hour": 2, "day": 25, "source": "predicted" }
+  ],
+  "humidity": [
+    { "value": 35, "hour": 24, "day": 24, "source": "real" },
+    { "value": 34, "hour": 1, "day": 25, "source": "predicted" }
+    { "value": 33, "hour": 2, "day": 25, "source": "predicted" }
+  ],
+  "forecast": "Here is your detailed forest forecast text.",
+  "fire_alarm": false,
+  "condition": "night"
+}
+
+Ensure that the "forecast" field provides valuable insights into the weather conditions and any recommendations. The "condition" field should represent the current weather condition in the forest, which can ONLY be "sunny," "rainy," "stormy," or "night."
+
+Return this JSON object as your response, and do not include any additional text, explanations, or notes before or after the JSON object.
+
+You must follow this JSON format strictly. If the response format is not JSON or does not match the specified structure, please retry until it does.
+
+Thank you for your assistance!
+`;
+
+  let responseValid = false;
 
   while (!responseValid) {
     try {
