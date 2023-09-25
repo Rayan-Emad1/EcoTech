44a2671d085829b7844e4227949f8bfcb9e28db5
Rayan <rayanemad508@gmail.com>
2023-09-25 22:03:19 +0300

    AI <> added test function to verify respond type

--- Server/configs/open.ai.js
+++ Server/configs/open.ai.js
@@ -11,13 +11,35 @@ const openai = new OpenAI({
 
 // Prediction function
 const OpenAiPrediction = async (input) => {
-  // Get OpenAI response
-  const response = await openai.completions.create({
-    model: "text-davinci-003",
-    prompt: "say test",
-    temperature: 0.7,
-    max_tokens: 500,
-  });
+
+
+      // Get OpenAI response
+      const response = await openai.completions.create({
+        model: "text-davinci-003",
+        prompt: prompt,
+        temperature: 0.7,
+        max_tokens: 500,
+      });
+
+      // Check if the response is valid JSON
+      const isValidJson = (text) => {
+        try {
+          JSON.parse(text);
+          return true;
+        } catch (error) {
+          return false;
+        }
+      };
+
+      // Validate JSON
+      if (isValidJson(response.choices[0].text)) {
+        responseValid = true;
+        const json = JSON.parse(response.choices[0].text.trim());
+        return json;
+      } else {
+        console.log("Invalid response, retrying...");
+      }
+    
 };
 
 // Export function
