5eaa095419df66d635d5ca19e6a76f0fc8ca66a3
Rayan <rayanemad508@gmail.com>
2023-09-25 21:24:18 +0300

    Arduino <> created cpp file and added libraries

---
+++ Arduino/main.cpp
@@ -0,0 +1,22 @@
+#include "DHT.h"
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
+#include <ESP8266HTTPClient.h>
+#include <TimeLib.h>
+
+
+
+
+void setup() {
+
+}
+
+void loop() {
+  // Read temperature and humidity every minute
+
+
+
+}
+
+
+
